6c0c0fc1a77a309ba0e08e761cdc5289b5f491e4
Aakif Rashid <aakif@Aakifs-MacBook-Air.local>
2022-10-11 12:04:26 +0530

    My First Commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.sql.Array;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+
+        System.out.println("Hello world");
+        System.out.println("aakif");
+
+        //saima is a bad boy
+
+        int age6 = 123;
+        long age2 =12345678900L;
+
+    float pie = 3.14F;
+
+// concatenate
+    String name1 = "@@kif";
+        String name2 = " >3";
+                String name3 = name1 +name2 ;
+       // System.out.println(name3);
+        int arr[]=new int[4];
+
+        arr[2]=56;
+        System.out.println(arr[0]);
+
+        int age = 15;
+        if (age<19){
+            System.out.println("under age");
+        }
+        else{
+            System.out.println("above 18");
+        }
+
+        System.out.println("saima is a bad boy"
+        );
+
+    }
+}
